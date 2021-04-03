7f574b7847026e331dc5071eff6511318ac8d436
Thomas Beiganz <thomas.beiganz@gmail.com>
2021-04-03 17:09:39 +0200

    init

---
+++ bot/src/main/java/se/wastedtime/ts3/SoundBotApplication.java
@@ -0,0 +1,13 @@
+package se.wastedtime.ts3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoundBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoundBotApplication.class, args);
+	}
+
+}
---
+++ bot/src/test/java/se/wastedtime/ts3/SoundBotApplicationTests.java
@@ -0,0 +1,13 @@
+package se.wastedtime.ts3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoundBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
